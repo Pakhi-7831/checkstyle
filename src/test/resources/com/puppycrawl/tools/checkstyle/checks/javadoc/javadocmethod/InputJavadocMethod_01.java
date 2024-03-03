1432de085aba0322239efb472f390719b5f6ac3b
Pakhi-7831 <101047461+Pakhi-7831@users.noreply.github.com>
2024-03-03 18:56:43 +0530

    Issue #13213: Remove"//ok"from InputJavadocMethod1

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethod_01.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/javadoc/javadocmethod/InputJavadocMethod_01.java
@@ -17,35 +17,35 @@ public class InputJavadocMethod_01 {
     /** Do 1.
      * @throws TestException1 when problem occurs.
      */
-    public void doStuff1() throws TestException1 { 
+    public void doStuff1() throws TestException1 {
         try {
-            doStuff2(); 
+            doStuff2();
         } catch (final TestException2 e) { }
-        throw new InputJavadocMethod_01().new TestException1(""); 
+        throw new InputJavadocMethod_01().new TestException1("");
     }
     /** Do 2.
      * @throws TestException2 when problem occurs.
      */
-    private static void doStuff2() throws TestException2 { 
-        throw new TestException2(""); 
+    private static void doStuff2() throws TestException2 {
+        throw new TestException2("");
     }
     /** Exception 1.
      */
-    class TestException1 extends Exception { 
+    class TestException1 extends Exception {
         /** Exception 1.
          * @param messg message
          */
-        TestException1(String messg) { 
+        TestException1(String messg) {
             super(messg);
         }
     }
     /** Exception 2.
      */
-    public static class TestException2 extends Exception { 
+    public static class TestException2 extends Exception {
         /** Exception 2.
          * @param messg message
          */
-        TestException2(String messg) { 
+        TestException2(String messg) {
             super(messg);
         }
     }
